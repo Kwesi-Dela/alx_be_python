e788b87778ab1036a231e301600e504124b617e3
Kwesi-Dela <kwesidelaodoom@gmail.com>
2025-07-21 19:50:53 +0000

    hours to secs convertor dynamic formulate and print

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,11 @@
+hours=2
+#1 hour is 60 minutes
+
+#1 minute is 60 seconds.
+#60 * 60 = 3600 seconds.
+
+# Convert hours to seconds
+seconds = hours * 3600
+
+# the result
+print(f"{hours} hour(s) is {seconds} seconds.")
